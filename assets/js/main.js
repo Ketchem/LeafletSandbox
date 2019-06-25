2f32433717a69bf43caf2c1c569d92f9bcc4b64c
Brian K <>
2019-06-25 13:39:28 -0400

    Updated to make map full screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,38 @@
 // DEFINE GLOBAL VARIABLES
 // mapbox access token
 var accessToken = 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg';
-
-//  mapbox tiles
-// https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?title=true&access_token=pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg#6.1/44.622012/-89.976147/0
-// https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?fresh=true&title=true&access_token=pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg#6.1/44.622012/-89.976147/0
-// var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?access_token=' + accessToken + '#6.1/44.622012/-89.976147/0', {
-//     attribution: '<a href="https://www.mapbox.com/feedback/">Mapbox</a> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// });
-
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + accessToken, {
-    attribution: '<a href="https://www.mapbox.com/feedback/">Mapbox</a> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+mapboxgl.accessToken = accessToken;
 
 // create the map object and set the center and zoom
-var map = L.map('map', {
+// var map = L.map('map', {
+//     center: [44.75, -89.65],
+//     zoom: 7,
+//     hash: true,
+//     transformRequest: (url, resourceType)=> {
+//       if(resourceType === 'Source' && url.startsWith('http://localhost')) {
+//         return {
+//          url: url.replace('http', 'https'),
+//          headers: { 'my-custom-header': true},
+//          credentials: 'include'  // Include cookies for cross-origin requests
+//        }
+//       }
+//     }
+// });
+
+var map = new mapboxgl.Map({
+    container: 'map',
     center: [44.75, -89.65],
-    zoom: 7
-});
+    zoom: 7,
+    // style: 'mapbox://styles/ketchem2/cjxaofh553ssx1cpdh6nkjqo3',
+    style: 'mapbox://styles/mapbox/dark-v9',
+    hash: true //,
+    // transformRequest: (url, resourceType)=> {
+    //   if(resourceType === 'Source' && url.startsWith('http://localhost')) {
+    //     return {
+    //      url: url.replace('http', 'https'),
+    //      headers: { 'my-custom-header': true},
+    //      credentials: 'include'  // Include cookies for cross-origin requests
+    //    }
+    //   }
+    // }
+  });
